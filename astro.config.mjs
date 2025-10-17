4ed81543b4a727c7a796360e45a436abf89a3d94
Luke Hallett <lha189@uclive.ac.nz>
2025-10-17 13:46:39 +1300

    chore: add Cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,40 +3,44 @@ import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 import starlightThemeObsidian from 'starlight-theme-obsidian'
 
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-            plugins: [starlightThemeObsidian({
-                backlinks: false,
-                graph: false,
-            })],
-			title: 'docs.lukeh.nz',
-			sidebar: [
-                {
-                    label: 'Home',
-                    slug: ''
-                },
-                {
-                    label: 'Deployment',
-                    items: [
-                        { label: 'Alpine Linux', slug: 'deployment/alpine-linux' },
-                        { label: 'Ubuntu', slug: 'deployment/ubuntu' },
-                    ],
-                },
-                {
-                    label: 'Programming',
-                    items: [
-                        { label: 'Laravel', slug: 'programming/laravel' },
-                    ],
-                },
-                {
-                    label: 'General',
-                    items: [
-                        { label: 'macOS', slug: 'general/macos' },
-                    ],
-                },
-			],
-		}),
+  integrations: [
+      starlight({
+          plugins: [starlightThemeObsidian({
+              backlinks: false,
+              graph: false,
+          })],
+          title: 'docs.lukeh.nz',
+          sidebar: [
+              {
+                  label: 'Home',
+                  slug: ''
+              },
+              {
+                  label: 'Deployment',
+                  items: [
+                      { label: 'Alpine Linux', slug: 'deployment/alpine-linux' },
+                      { label: 'Ubuntu', slug: 'deployment/ubuntu' },
+                  ],
+              },
+              {
+                  label: 'Programming',
+                  items: [
+                      { label: 'Laravel', slug: 'programming/laravel' },
+                  ],
+              },
+              {
+                  label: 'General',
+                  items: [
+                      { label: 'macOS', slug: 'general/macos' },
+                  ],
+              },
+          ],
+      }),
 	],
+
+  adapter: cloudflare(),
 });
